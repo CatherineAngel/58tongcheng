2c9bc6e46c468f4cf13c3a7c1173c45c9158b58d
CatherineAngel <38883357+CatherineAngel@users.noreply.github.com>
2018-12-15 13:57:48 +0800

    更改
    对base64的parse解密后，对于主程序的修改

--- Spider.py
+++ Spider.py
@@ -4,6 +4,7 @@ from lxml import etree
 
 import time
 
+import base64_parse
 from pymongo import MongoClient
 import dbmongo
 
@@ -18,40 +19,19 @@ headers={
 }
 def url_parse(url):
     res = requests.get(url)
-
-
-
     html=etree.HTML(res.text)
-
-    # print(html)
-
     list=html.xpath("//div[@class='img_list']/a/@href")
-
     return list
 
-
-
-
-# def parse(urls):
-# urls='https://xa.58.com/zufang/36330842219927x.shtml?entinfo=36330842219927_0&fzbref=0&from=1-list-0&params=rankbusitime0099^desc&psid=185901523202455494597525941&iuType=gz_2&ClickID=2&cookie=||https://www.baidu.com/link?url=pOZjCUPcqs4a7auBYHbLk4LEXF9dXjym6rZuIcKrmMS&wd=&eqid=d5b3e75900051106000000025c10ebe1|c5/njVwPfg2qV%20hQCT15Ag==&PGTID=0d3090a7-001e-3e0b-7109-d5a1e167f2c5&apptype=0&key=&pubid=53018024&trackkey=36330842219927_4d508b8c-9cc3-4e7d-a9d1-de2e4f04d223_20181212192108_1544613668607&fcinfotype=gz'
-
-
-
-
 def parse(html):
-
     data={}
-
     #宣传语
     try:
         data['tagline'] = html.xpath("//div[@class='house-title']/h1/text()")
-
         #房价
-        data['price'] = html.xpath("//div[@class='house-pay-way f16']/span/b/text()")
+        data['price'] = base64_parse.parse_price(html) #调用反扒base64中的解析函数
         #议价方式
-
         data['议价方式']=html.xpath("//div[@class='house-pay-way f16']/span[@class='c_333']/text()")
-
         # 租赁方式：
         # 房屋类型：
         # 朝向楼层：
@@ -59,8 +39,8 @@ def parse(html):
         # 所属区域：
         # 详细地址：
         data['lease'] = ''.join(html.xpath("//div[@class='house-desc-item fl c_333']/ul/li[1]/span[2]/text()")[0].split())
-        data['Type'] = ''.join(html.xpath("//div[@class='house-desc-item fl c_333']/ul/li[2]/span[2]/text()")[0].split())
-        data['toward'] = ''.join(html.xpath("//div[@class='house-desc-item fl c_333']/ul/li[3]/span[2]/text()")[0].split())
+        data['Type'] = base64_parse.parse_type(html)
+        data['toward'] = base64_parse.parse_toward(html)
         data['plot'] = ''.join(html.xpath("//div[@class='house-desc-item fl c_333']/ul/li[4]/span[2]/a/text()")[0].split())
         data['area'] = (''.join(html.xpath("string(//div[@class='house-desc-item fl c_333']/ul/li[5])").split())).replace('所属区域：','')
         data['address']=''.join(html.xpath("//div[@class='house-desc-item fl c_333']/ul/li[6]/span[2]/text()")[0].split())
@@ -84,9 +64,8 @@ def parse(html):
 if __name__=='__main__':
     #start_url = 'https://xa.58.com/chuzu/?PGTID=0d100000-001e-3005-fc0e-fc1b243fa56e&ClickID=2'
     # 实例化mongodb并连接
-
-    db=dbmongo.mongo()
-    for count in range(21,70):
+    # db=dbmongo.mongo()
+    for count in range(1,70):
         start_url = 'https://xa.58.com/chuzu/pn' + str(count) + '/?PGTID=0d100000-001e-3005-fc0e-fc1b243fa56e&ClickID=2'
         url_list = url_parse(start_url)
         print("第"+str(count)+"页的数据")
@@ -97,9 +76,10 @@ if __name__=='__main__':
             response = requests.get(url, headers=headers)
             html = etree.HTML(response.text)
             data = parse(html)
-            db.insert(data)
-            # with open('data.txt', 'a')  as f:
-            #     f.write(str(data) + '\n')
+            data['url']=url
+            # db.insert(data)
+            with open('data2.txt', 'a')  as f:
+                f.write(str(data) + '\n')
             print(data)
             time.sleep(2.5)
 
